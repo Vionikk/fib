77c1d1dc84df921c64b5263c440287fa236621c5
Vlad Shelin <vladsheln58@gmail.com>
2020-09-14 18:41:05 +0300

    first modification

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+def fib(n):
+    if(n==0):
+        return 0
+    if(n==1):
+        return 1
+    return fib(n-2) + fib(n-1)
+
+count = int(input("How many Fibonacci numbers you want?: "))
+
+for x in range(0, count):
+    print(fib(x))
